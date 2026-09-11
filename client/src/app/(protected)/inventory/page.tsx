c13379146517ec846f7c62ea455c047320d3405e
Hayyaanshakeel <ayyanshakeel899@gmail.com>
2026-09-10 21:38:16 -0400

    Day 78 : Adding view single inventory in frontend

--- client/src/app/(protected)/inventory/page.tsx
+++ client/src/app/(protected)/inventory/page.tsx
@@ -1,6 +1,28 @@
 import React from 'react'
-
+import {useEffect,useState} from 'react'
+import {getSingleInventory} from "@/services/inventoryService"
+import { useParams } from 'next/navigation'
+import { Inventory } from '@/types/inventory'
 export default function Inventory() {
+  const [inventory, setInventory] = useState< Inventory | null>(null);
+  
+    const params = useParams();
+    const id = params.id as string;
+  
+  useEffect(()=>{
+    const getSinglealoneInventory = async(){
+      try{
+        const response = await getSingleInventory(id)
+        if(response){
+          setInventory(response)
+        }
+      }
+      catch(error){
+        console.error(error)
+      }
+    }
+    getSinglealoneInventory()
+  },[id])
   return (
     <div>page</div>
   )
