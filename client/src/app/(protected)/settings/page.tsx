046fd17be71006e6e7cdad6d8e65c3ca344640ee
Hayyaanshakeel <ayyanshakeel899@gmail.com>
2026-09-12 20:59:41 -0400

    Day 80 : Added settings page in our frontend

---
+++ client/src/app/(protected)/settings/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Settings() {
+  return (
+    <div  className="text-center font-bold mt-10 text-4xl ">Settings will be avilable soon</div>
+  )
+}
